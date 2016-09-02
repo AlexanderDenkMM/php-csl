4f30652e0f6f8ec4301fb9f07af84dade49cb66d
Alexander Denk <alexander.denk@mymuesli.com>
2016-09-02 08:45:59 +0200

    Update functions.php

--- ds/functions.php
+++ ds/functions.php
@@ -1,5 +1,15 @@
 <?php
 
+namespace csl\ds;
+
 function s ($string) {
     return new _String($string);
+}
+
+function i ($integer) {
+    return new _Integer($integer);
+}
+
+function a (array $array) {
+    return new _Array($array);
 }
