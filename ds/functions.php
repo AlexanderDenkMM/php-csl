ad1afb3a41fcc98203c2319f04769705f5986f66
Alexander Denk <alexander.denk@mymuesli.com>
2016-09-06 10:23:55 +0200

    Update functions.php

--- ds/functions.php
+++ ds/functions.php
@@ -2,14 +2,26 @@
 
 namespace csl\ds;
 
-function s ($string) {
-    return new _String($string);
-}
-
 function i ($integer) {
     return new _Integer($integer);
 }
 
+function f ($float) {
+    return new _Float($float);
+}
+
+function s ($string) {
+    return new _String($string);
+}
+
 function a (array $array) {
     return new _Array($array);
+}
+
+function o ($object) {
+    return new _Object($object);
+}
+
+function j ($json) {
+    return new _Json($json);
 }
